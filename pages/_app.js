e94851466ea5eade64def555d7b1d75d95f10bb8
rafi <rafi.alfatih@gmail.com>
2021-11-28 09:45:05 +0700

    add something

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import { Layout } from "../components";
+import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.querySelector("body").classList.add("subpixel-antialiased");
+  });
+
   return (
     <Layout>
       <Component {...pageProps} />

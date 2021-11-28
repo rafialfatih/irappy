c6925012220ed958f06d56ef5d47e33c07b87696
rafi <rafi.alfatih@gmail.com>
2021-11-28 11:29:13 +0700

    fix mobile hader

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
         <link rel="icon" href="/apple.ico" />
       </Head>
       <div className="gradient-bg">
-        <div className="w-full mx-auto">
-          <div className="flex items-center mx-5 px-5 pt-4 pb-2">
+        <div className="w-full mx-auto pt-2">
+          <div className="flex justify-center md:justify-start items-center mx-5 px-5 pt-4 pb-2">
             <div className="flex items-center">
               <Link href="/">
                 <a className="text-gray-100 font-bold text-xl">
@@ -24,17 +24,23 @@ const Header = () => {
                   />
                 </a>
               </Link>
-              <Link href="/">
-                <a className="text-gray-100 text-base md:text-lg ml-8">
-                  Profile
-                </a>
-              </Link>
-              <Link href="/">
-                <a className="text-gray-100 text-base md:text-lg ml-5">About</a>
-              </Link>
-              <Link href="blog">
-                <a className="text-gray-100 text-base md:text-lg ml-5">Blog</a>
-              </Link>
+              <div className="hidden md:block">
+                <Link href="/">
+                  <a className="text-gray-100 text-base md:text-lg ml-8">
+                    Profile
+                  </a>
+                </Link>
+                <Link href="/">
+                  <a className="text-gray-100 text-base md:text-lg ml-5">
+                    About
+                  </a>
+                </Link>
+                <Link href="blog">
+                  <a className="text-gray-100 text-base md:text-lg ml-5">
+                    Blog
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </div>

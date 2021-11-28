26a438e166dd43584c20424661e0af9fccd0d1c2
rafi <rafi.alfatih@gmail.com>
2021-11-28 09:14:05 +0700

    fix font size, margin, and  image size  on mobile

--- components/CardProduct.jsx
+++ components/CardProduct.jsx
@@ -32,53 +32,41 @@ const variantRight = {
 const CardProduct = () => {
   return (
     <>
-      <div className="md:flex mx-5 my-3 grid md:grid-cols-2 gap-4">
+      <div className="text-center md:flex mx-5 my-3 grid md:grid-cols-2 gap-4">
         <motion.div
           variants={variantLeft}
           initial="hidden"
           whileInView="visible"
-          viewport={{ amount: 0.1 }}
-          className="overflow-hidden md:h-screen md:w-1/2 py-4 bg-white shadow-lg rounded-lg"
+          className="overflow-hidden md:h-screen md:w-1/2 pt-4 bg-white shadow-lg rounded-lg"
         >
-          <h2 className="text-gray-800 text-2xl md:text-3xl text-center my-5 mx-8 font-semibold">
+          <h2 className="text-gray-800 text-2xl md:text-3xl text-center mt-5 mx-8 font-semibold">
             AirPods
           </h2>
-          <img src="/img/airpods_07.png" className="" alt="Air Pods" />
-          <p className="mt-2 text-gray-600 mx-8">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-            dolores deserunt ea doloremque natus error, rerum quas odio quaerat
-            nam ex commodi hic, suscipit in a veritatis pariatur minus
-            consequuntur!
-          </p>
-          <div className="flex justify-center mt-4">
+          <p className="mt-1 mx-8">Sepenuhnya baru dengan audio spasial.</p>
+          <div className="flex justify-center mt-1">
             <a
               href="#"
-              className="text-lg md:text-xl no-underline text-blue-700 hover:underline"
+              className="text-md md:text-lg no-underline text-blue-700 hover:underline"
             >
               Selengkapnya
             </a>
           </div>
+          <img src="/img/airpods_07.png" className="mt-9" alt="Air Pods" />
         </motion.div>
         <motion.div
           variants={variantRight}
           initial="hidden"
           whileInView="visible"
-          viewport={{ amount: 0.1 }}
-          className="overflow-hidden md:h-screen md:w-1/2 py-4 px-8 bg-white shadow-lg rounded-lg"
+          className="overflow-hidden md:h-screen md:w-1/2 pt-4 px-8 bg-white shadow-lg rounded-lg"
         >
-          <h2 className="text-gray-800 text-2xl md:text-3xl my-5 text-right font-semibold">
+          <h2 className="text-gray-800 text-2xl md:text-3xl mt-5 mb-1 font-semibold">
             iPad Mini
           </h2>
-          <p className="mt-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-            dolores deserunt ea doloremque natus error, rerum quas odio quaerat
-            nam ex commodi hic, suscipit in a veritatis pariatur minus
-            consequuntur!
-          </p>
-          <div className="flex justify-center mt-4">
+          <p className="mt-1">Kekuatan mumpuni. Ukuran mini.</p>
+          <div className="flex justify-center mt-1">
             <a
               href="#"
-              className="text-lg md:text-xl no-underline text-blue-700 hover:underline"
+              className="text-md md:text-lg no-underline text-blue-700 hover:underline"
             >
               Selengkapnya
             </a>
@@ -86,28 +74,22 @@ const CardProduct = () => {
           <img src="/img/ipad_mini.png" alt="Air Pods" />
         </motion.div>
       </div>
-      <div className="md:flex mx-5 my-3 grid md:grid-cols-2 gap-4">
+      <div className="text-center md:flex mx-5 my-3 grid md:grid-cols-2 gap-4">
         <motion.div
           variants={variantRight}
           initial="hidden"
           whileInView="visible"
-          viewport={{ amount: 0.1 }}
-          className="overflow-hidden md:h-screen md:w-1/2 py-4 px-8 bg-white shadow-lg rounded-lg"
+          className="overflow-hidden md:h-screen md:w-1/2 pt-4 px-8 bg-white shadow-lg rounded-lg"
         >
           <img src="/img/apple_watch.png" alt="Air Pods" />
-          <h2 className="text-gray-800 text-2xl md:text-3xl my-5 font-semibold">
+          <h2 className="text-gray-800 text-2xl md:text-3xl mt-4 mb-1 font-semibold">
             Apple Watch
           </h2>
-          <p className="mt-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-            dolores deserunt ea doloremque natus error, rerum quas odio quaerat
-            nam ex commodi hic, suscipit in a veritatis pariatur minus
-            consequuntur!
-          </p>
-          <div className="flex justify-center mt-4">
+          <p className="mt-1">Memperkenalkan layar terluas kami.</p>
+          <div className="flex justify-center mt-1">
             <a
               href="#"
-              className="text-lg md:text-xl no-underline text-blue-700 hover:underline"
+              className="text-md md:text-lg no-underline text-blue-700 hover:underline"
             >
               Selengkapnya
             </a>
@@ -117,22 +99,16 @@ const CardProduct = () => {
           variants={variantLeft}
           initial="hidden"
           whileInView="visible"
-          viewport={{ amount: 0.1 }}
-          className="overflow-hidden md:h-screen md:w-1/2 py-4 px-8 bg-white shadow-lg rounded-lg"
+          className="overflow-hidden md:h-screen md:w-1/2 pt-4 px-8 bg-white shadow-lg rounded-lg"
         >
-          <h2 className="text-gray-800 text-2xl md:text-3xl my-5 text-center my-2 font-semibold">
+          <h2 className="text-gray-800 text-2xl md:text-3xl mt-5 mb-1 text-center font-semibold">
             iPhone 13 Pro Max
           </h2>
-          <p className="mt-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-            dolores deserunt ea doloremque natus error, rerum quas odio quaerat
-            nam ex commodi hic, suscipit in a veritatis pariatur minus
-            consequuntur!
-          </p>
-          <div className="flex justify-center my-6">
+          <p className="mt-1">Kekuatan super baru di tangan Anda.</p>
+          <div className="flex justify-center mb-6 mt-1">
             <a
               href="#"
-              className="text-lg md:text-xl no-underline text-blue-700 hover:underline"
+              className="text-md md:text-lg no-underline text-blue-700 hover:underline"
             >
               Selengkapnya
             </a>
